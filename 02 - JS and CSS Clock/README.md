ee45fd81d7897ffd7aa6dcb6abaef93867f1ca21
Matsuko Friedland <info@matsuko.ca>
2017-11-04 22:55:25 -0700

    Start working on outstanding to-dos
    Check if value changed before updating it

--- 02 - JS and CSS Clock/README.md
+++ 02 - JS and CSS Clock/README.md
@@ -4,7 +4,7 @@ Functional analog clock.
 ## Changes made from original:
 - [x] Modify file structure so that it's a bit more organized
 - [x] Style clock hands so they can be visually differentiated without waiting for hands to move
-- [ ] ~~ Style clock so it has numbers and lines? ~~ Decided against it because I don't want to spend time making assets.
+- [ ] ~~Style clock so it has numbers and lines?~~ Decided against it because I don't want to spend time making assets.
 - [x] Make JS variable names more consistent (i.e. plural/singular, abbreviations)
 - [x] Adjust the styling instead of adding 90(deg) in JS
 - [ ] Fix "jump" in animation when hand gets to 12 o'clock
--- 02 - JS and CSS Clock/js/modified.js
+++ 02 - JS and CSS Clock/js/modified.js
@@ -3,25 +3,64 @@ const minuteHand = document.querySelector('.hand--minute');
 const hourHand = document.querySelector('.hand--hour');
 const digitalClock = document.querySelector('.digital-time');
 
+let previousHour = 0;
+let previousMinute = 0;
+let previousSecond = 0;
+
 function setTime() {
   const now = new Date();
-
-  // Update analog clock
+  const hour = now.getHours();
+  const minute = now.getMinutes();
   const second = now.getSeconds();
+
+  setAnalogClock(hour,minute,second);
+  setDigitalClock(hour,minute,second);
+}
+
+function setSecond(second) {
+
+  if(previousSecond == second) {
+    return;
+  }else {
+    previousSecond = second;
+  }
+
   const secondDegrees = ((second / 60) * 360);
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+}
 
-  const minute = now.getMinutes();
+function setMinute(minute) {
+  if(previousMinute == minute) {
+    return;
+  }else {
+    previousMinute = minute;
+  }
+
   const minuteDegrees = ((minute / 60) * 360);
   minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+}
 
-  const hour = now.getHours();
+function setHour(hour) {
+
+  if(previousHour == hour) {
+    return;
+  }else {
+    previousHour = hour;
+  }
+
   const hourDegrees = ((hour / 12) * 360);
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
 
-  // Update digital clock
-  const time = `${hour}:${minute}:${second}`;
-  digitalClock.innerHTML = time;
+function setAnalogClock(hour,minute,second) {
+  setHour(hour);
+  setMinute(minute);
+  setSecond(second);
+}
+
+function setDigitalClock(hour,minute,second) {
+  const newTime = `${hour}:${minute}:${second}`;
+  digitalClock.innerHTML = newTime;
 }
 
 setInterval(setTime, 1000);
