4685bbea18b5c91370070666ddbdecf57a3b34f6
Matsuko Friedland <info@matsuko.ca>
2017-11-04 21:54:52 -0700

    Add digital time for accessibility

--- 02 - JS and CSS Clock/README.md
+++ 02 - JS and CSS Clock/README.md
@@ -4,8 +4,9 @@ Functional analog clock.
 ## Changes made from original:
 - [x] Modify file structure so that it's a bit more organized
 - [x] Style clock hands so they can be visually differentiated without waiting for hands to move
-- [ ] Style clock so it has numbers and lines?
+- [ ] ~~ Style clock so it has numbers and lines? ~~ Decided against it because I don't want to spend time making assets.
 - [x] Make JS variable names more consistent (i.e. plural/singular, abbreviations)
 - [x] Adjust the styling instead of adding 90(deg) in JS
 - [ ] Fix "jump" in animation when hand gets to 12 o'clock
-- [ ] Add text value of time for accessibility
+- [x] Add text value of time for accessibility
+- [ ] Only change transform/content when it is not the same as previous
--- 02 - JS and CSS Clock/js/modified.js
+++ 02 - JS and CSS Clock/js/modified.js
@@ -1,10 +1,12 @@
 const secondHand = document.querySelector('.hand--second');
 const minuteHand = document.querySelector('.hand--minute');
 const hourHand = document.querySelector('.hand--hour');
+const digitalClock = document.querySelector('.digital-time');
 
 function setTime() {
   const now = new Date();
 
+  // Update analog clock
   const second = now.getSeconds();
   const secondDegrees = ((second / 60) * 360);
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
@@ -16,6 +18,10 @@ function setTime() {
   const hour = now.getHours();
   const hourDegrees = ((hour / 12) * 360);
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  // Update digital clock
+  const time = `${hour}:${minute}:${second}`;
+  digitalClock.innerHTML = time;
 }
 
 setInterval(setTime, 1000);
