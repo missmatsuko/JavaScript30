95d0d5fc5c3d94486c850c1d54062bb06f0a77f3
Matsuko Friedland <info@matsuko.ca>
2017-11-26 21:13:56 -0800

    Use classList.toggle instead of add/remove

--- 13 - Slide in on Scroll/README.md
+++ 13 - Slide in on Scroll/README.md
@@ -3,5 +3,8 @@ Slide in image elements by scrolling the page.
 
 ## Changes made from original:
 - [x] Modify file structure so that it's a bit more organized
+- [ ] Create separate debounced checkSlide function so it doesn't run once on page load
+- [x] Make variables for repeated values
+- [x] Use `classList.toggle` instead of `add/remove`
 
 ## Things I learned:
--- 13 - Slide in on Scroll/js/modified.js
+++ 13 - Slide in on Scroll/js/modified.js
@@ -12,3 +12,24 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const activeClass = 'active';
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  sliderImages.forEach(sliderImage => {
+
+    const windowScrollY = window.scrollY;
+    const imageHeight = sliderImage.height;
+    const imageTop = sliderImage.offsetTop;
+    const imageBottom = sliderImage.offsetTop + imageHeight;
+
+    const slideInAt = (windowScrollY + window.innerHeight) - (imageHeight / 2);
+    const isHalfShown = slideInAt > imageTop;
+    const isNotScrolledPast = windowScrollY < imageBottom;
+
+    sliderImage.classList.toggle(activeClass, isHalfShown && isNotScrolledPast);
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
