5a3b964d63d0cb9f0bf680fbcc53f9fbfe7e19b4
Matsuko Friedland <info@matsuko.ca>
2017-12-13 22:50:49 -0800

    Do day 17 challenge

--- 17 - Sort Without Articles/README.md
+++ 17 - Sort Without Articles/README.md
@@ -2,5 +2,6 @@
 
 ## Changes made from original:
 - [x] Modify file structure so that it's a bit more organized
+- [x] Changed some variable/function names
 
 ## Things I learned:
--- 17 - Sort Without Articles/js/modified.js
+++ 17 - Sort Without Articles/js/modified.js
@@ -1,1 +1,12 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const sortedBands = bands.sort((a, b)=>(stripArticles(a) > stripArticles(b) ? 1 : -1));
+
+function stripArticles(name) {
+  return name.replace(/^(a |the |an )/i, '').trim();
+}
+
+document.querySelector('#bands').innerHTML =
+  sortedBands
+    .map(band => `<li>${band}</li>`)
+    .join('');
