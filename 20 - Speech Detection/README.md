e8cddc532fa090e45e63c6748180144d98ce48e4
Matsuko Friedland <info@matsuko.ca>
2017-12-17 15:00:52 -0800

    Do day 20 challenge

--- 20 - Speech Detection/README.md
+++ 20 - Speech Detection/README.md
@@ -3,6 +3,8 @@ Transcribe speech.
 
 ## Changes made from original:
 - [x] Modify file structure so that it's a bit more organized
+- [x] Pull `result[0]` into its own variable for re-use.
 
 ## Things I learned:
-- There is native speech detection in JS, without needing any external libraries
+- There is native speech detection in JS, without needing any external libraries (although it's currently only available on Chrome and mobile Firefox)
+- How to use the speech detection feature
--- 20 - Speech Detection/js/modified.js
+++ 20 - Speech Detection/js/modified.js
@@ -1,1 +1,27 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+const words = document.querySelector('.words');
+let p = document.createElement('p');
+
+words.append(p);
+
+recognition.addEventListener('result', (e) => {
+  const result = e.results[0];
+
+  const transcript = Array.from(result)
+    .map(result => result.transcript)
+    .join(' ');
+
+  p.textContent = transcript;
+
+  if(result.isFinal) {
+    p = document.createElement('p');
+    words.appendChild(p);
+  }
+});
+
+recognition.start();
+recognition.addEventListener('end', recognition.start);
