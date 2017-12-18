ab7149831f4b5f60a76d1985de0ba7bb4b72bde2
Matsuko Friedland <info@matsuko.ca>
2017-12-17 17:24:47 -0800

    Do day 23 challenge

--- 23 - Speech Synthesis/README.md
+++ 23 - Speech Synthesis/README.md
@@ -2,5 +2,10 @@
 
 ## Changes made from original:
 - [x] Modify file structure so that it's a bit more organized
+- [ ] Figure out why selecting a Google voice makes the select reset to the first option (Chrome)
+- [x] Make speech start only when clicking speak button (not on settings changing)
 
 ## Things I learned:
+- There's native speech synthesis functionality in browsers, and it's supported by most modern browsers.
+- How to use the speech synthesis API
+- That foreign voices will still read English, with an accent. I expected it to just be really buggy/bad.
--- 23 - Speech Synthesis/js/modified.js
+++ 23 - Speech Synthesis/js/modified.js
@@ -2,5 +2,47 @@ const msg = new SpeechSynthesisUtterance();
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+const speakButton = document.querySelector('#speak');
+
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+
+  const voiceOptions = voices
+    .map((voice) => (`<option value="${voice.name}">${voice.name} (${voice.lang})</option>`))
+    .join('');
+
+  voicesDropdown.innerHTML = voiceOptions;
+}
+
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+
+  toggle(false);
+}
+
+function setOption() {
+  msg[this.name] = this.value;
+
+  toggle(false);
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+
+  if(startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+voicesDropdown.addEventListener('change', setVoice);
+
+options.forEach(option => option.addEventListener('change', setOption));
+
+stopButton.addEventListener('click', () => toggle(false));
+
+speakButton.addEventListener('click', toggle);
